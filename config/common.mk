PalladiumOS-Temp/platform_vendor_palladium

vendor: copy backuptool files to system
For some reason in android11 it fails to copy to $OUT/install, so copy to system as a temporary workaround till the root cause is discovered

config/common.mk
@@ -27,8 +27,8 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/palladium/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/palladium/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/palladium/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/palladium/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/palladium/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)

NezukoOS/vendor_aosp

config/common.mk
@@ -27,8 +27,8 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/nezuko/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/nezuko/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/nezuko/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/nezuko/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/nezuko/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
 
 # Font service Permissions